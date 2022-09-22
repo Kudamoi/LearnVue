09ceb57957978cd3852b301039d5a1090362b660
Александр Гавриленко <lihoy.sasha@gmail.com>
2022-09-22 22:45:06 +0300

    t2 edit to vue-cli

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,106 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section class="catalog">
+    <ul class="catalog__list">
+      <li class="catalog__item" v-for="product in products" :key="product.title">
+        <a class="catalog__pic" href="#">
+          <img :alt="product.title" :src="product.image" :srcset="product.image2x">
+        </a>
+
+        <h3 class="catalog__title">
+          <a href="#">
+            {{ product.title }}
+          </a>
+        </h3>
+
+        <span class="catalog__price">
+              {{ product.price }} ₽
+            </span>
+
+        <ul class="colors colors--black">
+          <li class="colors__item">
+            <label class="colors__label">
+              <input checked="" class="colors__radio sr-only" name="color-1" type="radio" value="#73B6EA">
+              <span class="colors__value" style="background-color: #73B6EA;">
+                  </span>
+            </label>
+          </li>
+          <li class="colors__item">
+            <label class="colors__label">
+              <input class="colors__radio sr-only" name="color-1" type="radio" value="#8BE000">
+              <span class="colors__value" style="background-color: #8BE000;">
+                  </span>
+            </label>
+          </li>
+          <li class="colors__item">
+            <label class="colors__label">
+              <input class="colors__radio sr-only" name="color-1" type="radio" value="#222">
+              <span class="colors__value" style="background-color: #222;">
+                  </span>
+            </label>
+          </li>
+        </ul>
+      </li>
+
+    </ul>
+
+    <ul class="catalog__pagination pagination">
+      <li class="pagination__item">
+        <a aria-label="Предыдущая страница" class="pagination__link pagination__link--arrow pagination__link--disabled">
+          <svg fill="currentColor" height="14" width="8">
+            <use xlink:href="#icon-arrow-left"></use>
+          </svg>
+        </a>
+      </li>
+      <li class="pagination__item">
+        <a class="pagination__link pagination__link--current">
+          1
+        </a>
+      </li>
+      <li class="pagination__item">
+        <a class="pagination__link" href="#">
+          2
+        </a>
+      </li>
+      <li class="pagination__item">
+        <a class="pagination__link" href="#">
+          3
+        </a>
+      </li>
+      <li class="pagination__item">
+        <a class="pagination__link" href="#">
+          4
+        </a>
+      </li>
+      <li class="pagination__item">
+        <a class="pagination__link" href="#">
+          ...
+        </a>
+      </li>
+      <li class="pagination__item">
+        <a class="pagination__link" href="#">
+          10
+        </a>
+      </li>
+      <li class="pagination__item">
+        <a aria-label="Следующая страница" class="pagination__link pagination__link--arrow" href="#">
+          <svg fill="currentColor" height="14" width="8">
+            <use xlink:href="#icon-arrow-right"></use>
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import products from '@/data/products';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      products,
+    };
   },
 };
 </script>
-
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
---
+++ src/data/products.js
@@ -0,0 +1,74 @@
+export default [
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+    image2x: '/img/radio@2x.jpg',
+  },
+  {
+    title: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
+    price: 5660,
+    image: '/img/toothbrush.jpg',
+    image2x: '/img/toothbrush@2x.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
+    price: 21790,
+    image: '/img/phone.jpg',
+    image2x: '/img/phone.webp',
+  },
+  {
+    title: 'Электроскейт Razor Cruiser',
+    price: 24690,
+    image: '/img/board.jpg',
+    image2x: '/img/board@2x.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Mi A3 4/64GB Android One',
+    price: 14960,
+    image: '/img/phone-2.jpg',
+    image2x: '/img/phone-2@2x.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Redmi 6/128GB',
+    price: 8960,
+    image: '/img/phone-3.jpg',
+    image2x: '/img/phone-3@2x.jpg',
+  },
+  {
+    title: 'Электрический дрифт-карт Razor Crazy Cart',
+    price: 39900,
+    image: '/img/bicycle.jpg',
+    image2x: '/img/bicycle@2x.jpg',
+  },
+  {
+    title: 'Гироскутер Razor Hovertrax 2.0',
+    price: 33900,
+    image: '/img/wheels.jpg',
+    image2x: '/img/wheels@2x.jpg',
+  },
+  {
+    title: 'Детский трюковой самокат Razor Grom',
+    price: 4990,
+    image: '/img/scooter.jpg',
+    image2x: '/img/scooter@2x.jpg',
+  },
+  {
+    title: 'Роллерсёрф Razor RipStik Air Pro',
+    price: 6990,
+    image: '/img/ripstik.jpg',
+    image2x: '/img/ripstik.webp',
+  },
+  {
+    title: 'Наушники AirPods с беспроводным зарядным футляром',
+    price: 16560,
+    image: '/img/airpods.jpg',
+    image2x: '/img/airpods@2x.jpg',
+  },
+  {
+    title: 'Наушники Sony',
+    price: 30690,
+    image: '/img/headphone.jpg',
+    image2x: '/img/headphone@2x.webp',
+  },
+];
