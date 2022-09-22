d9b5bbee5cd9be35b998c4a69d848852dc8fea16
Александр Гавриленко <lihoy.sasha@gmail.com>
2022-09-22 23:38:57 +0300

    t3 edit add productItem and ProductList components

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,6 @@
 <template>
   <section class="catalog">
-    <ul class="catalog__list">
-      <li class="catalog__item" v-for="product in products" :key="product.title">
-        <a class="catalog__pic" href="#">
-          <img :alt="product.title" :src="product.image" :srcset="product.image2x">
-        </a>
-
-        <h3 class="catalog__title">
-          <a href="#">
-            {{ product.title }}
-          </a>
-        </h3>
-
-        <span class="catalog__price">
-              {{ product.price }} ₽
-            </span>
-
-        <ul class="colors colors--black">
-          <li class="colors__item">
-            <label class="colors__label">
-              <input checked="" class="colors__radio sr-only" name="color-1" type="radio" value="#73B6EA">
-              <span class="colors__value" style="background-color: #73B6EA;">
-                  </span>
-            </label>
-          </li>
-          <li class="colors__item">
-            <label class="colors__label">
-              <input class="colors__radio sr-only" name="color-1" type="radio" value="#8BE000">
-              <span class="colors__value" style="background-color: #8BE000;">
-                  </span>
-            </label>
-          </li>
-          <li class="colors__item">
-            <label class="colors__label">
-              <input class="colors__radio sr-only" name="color-1" type="radio" value="#222">
-              <span class="colors__value" style="background-color: #222;">
-                  </span>
-            </label>
-          </li>
-        </ul>
-      </li>
-
-    </ul>
-
+    <ProductList :products="products"></ProductList>
     <ul class="catalog__pagination pagination">
       <li class="pagination__item">
         <a aria-label="Предыдущая страница" class="pagination__link pagination__link--arrow pagination__link--disabled">
@@ -94,8 +52,14 @@
 
 <script>
 import products from '@/data/products';
+// eslint-disable-next-line import/extensions
+import ProductList from '@/components/ProductList';
 
 export default {
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    ProductList,
+  },
   name: 'App',
   data() {
     return {
---
+++ src/components/ProductList.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul class="catalog__list">
+    <li v-for="product in products" :key="product.title" class="catalog__item">
+      <ProductItem :product="product"></ProductItem>
+    </li>
+  </ul>
+</template>
+
+<script>
+// eslint-disable-next-line import/extensions
+import ProductItem from '@/components/ProductItem';
+
+export default {
+  components: {
+    ProductItem,
+  },
+  props: ['products'],
+  name: 'ProductList',
+};
+</script>
+
+<style scoped>
+
+</style>
